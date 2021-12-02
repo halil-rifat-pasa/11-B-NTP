0a9f823cf86bc4c376049ad7103aebef8dbb0f2c
Hasan Kaya <35229371+HasanKaya53@users.noreply.github.com>
2021-12-02 16:11:24 +0300

    class yapısı
    class yapısı 3

---
+++ class-2/class-2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class ogrenciler
+        {
+            public string adi, soyadi, okulNumarasi;
+
+
+            public string MesajVer(string adi, string soyadi, string okulNumarasi)
+            {
+                //string mesaj = "Hoşgeldin " + this.adi + " " + this.soyadi + " Numaran:" + this.okulNumarasi;
+                string mesaj = "Hoşgeldin " + adi + " " + soyadi + " Numaran:" + okulNumarasi;
+                MessageBox.Show(mesaj);
+                return mesaj;
+            }
+
+
+        }
+
+
+        private void Form1_load(object sender, EventArgs e)
+        {
+            ogrenciler ogr = new ogrenciler();
+            ogr.adi = "Hasan";
+            ogr.soyadi = "Kaya";
+            ogr.okulNumarasi = "50";
+            ogr.MesajVer("Büşra", "Aydın", "51");
+            //string mesaj = ogr.MesajVer("Büşra","Aydın","51");
+            // MessageBox.Show(mesaj);
+        }
+    }
+}
---
+++ class-3/class-3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class islemler 
+        {
+
+            public int sayi1, sayi2;
+
+            public int toplamaYap() 
+            {
+                return this.sayi1 + this.sayi2;
+            }
+
+            public int cikarmaYap() 
+            {
+                return this.sayi1 - this.sayi2;
+            }
+
+            public int carpmaYap() 
+            {
+                return this.sayi1 * this.sayi2;
+            }
+
+            public double bolmeYap() 
+            {
+                return this.sayi1 / this.sayi2;
+            }
+        
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            islemler islem = new islemler();
+
+            try
+            {
+                islem.sayi1 = Convert.ToInt16(textBox1.Text);
+                islem.sayi2 = Convert.ToInt16(textBox2.Text);
+
+                string yapilacakIslem = comboBox1.Text;
+                string sonuc = "0";
+
+
+                if (yapilacakIslem == "+")
+                {
+                    sonuc = islem.toplamaYap().ToString();
+                }
+                else if (yapilacakIslem == "-")
+                {
+                    sonuc = islem.cikarmaYap().ToString();
+                }
+                else if (yapilacakIslem == "*")
+                {
+                    sonuc = islem.carpmaYap().ToString();
+                }
+                else if (yapilacakIslem == "/")
+                {
+                    sonuc = islem.bolmeYap().ToString();
+                }
+                else {
+                    sonuc = "Hatalı İşlem Seçimi";
+                }
+
+                textBox3.Text = sonuc;
+            }
+            catch {
+                MessageBox.Show("Hatalı bir şey yaptınız :(((");
+            }
+           
+
+            
+            
+
+        }
+    }
+}
