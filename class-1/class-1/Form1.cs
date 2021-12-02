442b4f2ba7d345408edb859b6b6bf18ad3f145c3
Hasan Kaya <35229371+HasanKaya53@users.noreply.github.com>
2021-12-02 10:55:45 +0300

    class
    classların oluşturulması

---
+++ class-1/class-1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Dikdortgen 
+        {
+            public int a, b;
+
+            public Dikdortgen(int a, int b) {
+                this.a = a;
+                this.b = b;
+             
+            }
+
+            public int AlanHesapla()
+            {
+                return this.a * this.b;
+            }
+
+            public int cevreHesapla()
+            {
+                return 2 * (this.a + this.b);
+            }
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dikdortgen DikdortgenClass = new Dikdortgen(10,20);
+            //MessageBox.Show(DikdortgenClass.a.ToString());
+            //MessageBox.Show(DikdortgenClass.a.ToString());
+         
+        }
+    }
+}
