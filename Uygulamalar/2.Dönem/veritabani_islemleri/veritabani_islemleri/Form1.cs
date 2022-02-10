3447060ceb1afa7e759d5fe27ff7c0c2beb6fb88
Hasan Kaya <35229371+HasanKaya53@users.noreply.github.com>
2022-02-10 10:51:51 +0300

    veritabanı listeleme sorgusu
    listeleme sorgusu yapımı

---
+++ Uygulamalar/2.Dönem/veritabani_islemleri/veritabani_islemleri/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace veritabani_islemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=_hasankaya;User ID=hasankaya;Password=123");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            
+            conn.Open(); //bağlantıyı aç
+            SqlCommand cmd = new SqlCommand("SELECT * FROM iller",conn);
+            SqlDataReader oku = cmd.ExecuteReader();
+
+            while(oku.Read())
+            {
+                listBox1.Items.Add("ID:"+oku["il_id_PK"].ToString() + " İl Adı: " + oku["il_adi"].ToString());
+                comboBox1.Items.Add("ID:" + oku["il_id_PK"].ToString() + " İl Adı: " + oku["il_adi"].ToString());
+                textBox2.Text = "ID:" + oku["il_id_PK"].ToString() + " İl Adı: " + oku["il_adi"].ToString();
+            }
+
+
+            conn.Close(); //bağlantıyı kapat..
+        }
+    }
+}
