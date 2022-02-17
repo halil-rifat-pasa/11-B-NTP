539c1aabf12764dc53d04b2b93fd9025e90795f9
Hasan Kaya <35229371+HasanKaya53@users.noreply.github.com>
2022-02-17 10:16:50 +0300

    Kullanıcı İşlemleri
    Giriş / Kayıt Ol yapımı

---
+++ Uygulamalar/2.Dönem/kullanici_islemleri/kullanici_islemle/yeniKayitForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace kullanici_islemle
+{
+    partial class yeniKayitForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.adiTextBox = new System.Windows.Forms.TextBox();
+            this.soyadiTextBox = new System.Windows.Forms.TextBox();
+            this.kullaniciAdiTextBox = new System.Windows.Forms.TextBox();
+            this.KullaniciSifreTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kayıt Ol";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kullanıcı Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kullanıcı Şifre:";
+            // 
+            // adiTextBox
+            // 
+            this.adiTextBox.Location = new System.Drawing.Point(85, 41);
+            this.adiTextBox.Name = "adiTextBox";
+            this.adiTextBox.Size = new System.Drawing.Size(241, 20);
+            this.adiTextBox.TabIndex = 2;
+            // 
+            // soyadiTextBox
+            // 
+            this.soyadiTextBox.Location = new System.Drawing.Point(85, 75);
+            this.soyadiTextBox.Name = "soyadiTextBox";
+            this.soyadiTextBox.Size = new System.Drawing.Size(241, 20);
+            this.soyadiTextBox.TabIndex = 2;
+            // 
+            // kullaniciAdiTextBox
+            // 
+            this.kullaniciAdiTextBox.Location = new System.Drawing.Point(85, 109);
+            this.kullaniciAdiTextBox.Name = "kullaniciAdiTextBox";
+            this.kullaniciAdiTextBox.Size = new System.Drawing.Size(241, 20);
+            this.kullaniciAdiTextBox.TabIndex = 2;
+            // 
+            // KullaniciSifreTextBox
+            // 
+            this.KullaniciSifreTextBox.Location = new System.Drawing.Point(85, 146);
+            this.KullaniciSifreTextBox.Name = "KullaniciSifreTextBox";
+            this.KullaniciSifreTextBox.Size = new System.Drawing.Size(241, 20);
+            this.KullaniciSifreTextBox.TabIndex = 2;
+            // 
+            // yeniKayitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 432);
+            this.Controls.Add(this.KullaniciSifreTextBox);
+            this.Controls.Add(this.kullaniciAdiTextBox);
+            this.Controls.Add(this.soyadiTextBox);
+            this.Controls.Add(this.adiTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "yeniKayitForm";
+            this.Text = "yeniKayitForm";
+            this.Load += new System.EventHandler(this.yeniKayitForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox adiTextBox;
+        private System.Windows.Forms.TextBox soyadiTextBox;
+        private System.Windows.Forms.TextBox kullaniciAdiTextBox;
+        private System.Windows.Forms.TextBox KullaniciSifreTextBox;
+    }
+}
---
+++ Uygulamalar/2.Dönem/kullanici_islemleri/kullanici_islemle/yeniKayitForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace kullanici_islemle
+{
+    public partial class yeniKayitForm : Form
+    {
+        public yeniKayitForm()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=_test;Persist Security Info=True;User ID=testUser;Password=123");
+
+        private void yeniKayitForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool kullaniciKontrolu = true;
+
+
+            conn.Open();
+            SqlCommand kullaniciBul = new SqlCommand("SELECT kullanici_adi FROM kullanicilar",conn);
+            SqlDataReader oku = kullaniciBul.ExecuteReader();
+
+            while (oku.Read())
+            {
+                if (oku["kullanici_adi"].ToString() == kullaniciAdiTextBox.Text) 
+                {
+                    kullaniciKontrolu = false;
+                }
+            }
+
+            conn.Close();
+
+
+
+            if (kullaniciKontrolu == true)
+            {
+                //kullanıcıyı kaydettim..
+                conn.Open();
+                SqlCommand kullaniciKaydet = new SqlCommand("INSERT INTO kullanicilar(adi,soyadi,kullanici_adi,kullanici_sifre) VALUES('" + adiTextBox.Text + "','" + soyadiTextBox.Text + "','" + kullaniciAdiTextBox.Text + "','" + KullaniciSifreTextBox.Text + "')", conn);
+                kullaniciKaydet.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Kullanıcı Sisteme Eklendi ..");
+
+            }
+            else {
+                MessageBox.Show("Böyle bir kullanıcı var ..");
+            }
+
+
+
+          
+        }
+    }
+}
